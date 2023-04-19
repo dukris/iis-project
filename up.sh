a5377d0d3d433d5197ab63e3bb9b850061003348
Hanna <theannalios@gmail.com>
2023-04-19 23:10:18 +0300

    Resolved #7

--- up.sh
+++ up.sh
@@ -3,7 +3,11 @@ cd src/infra
 kubectl apply -f pg-secrets.yml
 kubectl apply -f pg-stateful.yml
 kubectl apply -f pg-service.yml
+
 kubectl apply -f iis-secrets.yml
 kubectl apply -f iis-configmap.yml
 kubectl apply -f iis-deployment.yml
 kubectl apply -f iis-service.yml
+
+#minikube addons enable ingress
+kubectl apply -f ingress.yaml
